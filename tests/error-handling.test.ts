f988b04c5c47f00a5d73739ba2df0100010bf6c2
Japheth Kiptoo <thygon6422@gmail.com>
2025-03-11 12:45:04 +0300

    Update: tests

--- tests/error-handling.test.ts
+++ tests/error-handling.test.ts
@@ -1,5 +1,34 @@
+import UssdNavigator from "../src";
+import type { UssdNavigator as IUssdNavigator } from "../src/ussd-navigator";
+
+type UssdRespone = {
+  message: string;
+  end: boolean;
+};
+
 describe("Menu Error-handling", () => {
-  it('sum', () => {
-    expect(1).toBe(1);
-  })
-})
+  const router: IUssdNavigator<UssdRespone> = new UssdNavigator<UssdRespone>();
+
+  beforeEach(() => {
+    router.addMenu("start", {
+      run: () => {
+        return {
+          message: "Hello",
+          end: true,
+        };
+      },
+    });
+  });
+
+  test("Must have a start menu", async () => {
+    await expect(
+      router.run({
+        path: ["1"],
+        input: "",
+        sessionID: "",
+        phoneNumber: "",
+        serviceCode: "*280#",
+      }),
+    ).rejects.toThrow();
+  });
+});
